7e64803aa1112cae0904f4a12bfa06090b0d2466
aleksandar.dragas <aleksandar.dragas@ALEKSANDARD10>
2024-02-29 14:48:32 +0100

    Add project files.

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace API.Data
+{
+	public class DataContext : DbContext
+	{
+		public DataContext(DbContextOptions options):base(options) { }
+        public DbSet<AppUser> AppUsers { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+
+		}
+	}
+}
